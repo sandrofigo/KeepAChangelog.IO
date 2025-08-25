f75942edeecac70b1a1ee530ac27d26f18e11b92
Sandro Figo <sandro.figo@gmx.at>
2025-08-25 20:45:04 +0200

    Add nuke build

---
+++ build/Build.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Test);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetClean(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+            );
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetRestore(s => s
+                .SetProjectFile(Solution)
+            );
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+            );
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+            );
+        });
+}
