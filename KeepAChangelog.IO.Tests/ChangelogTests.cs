c60ad5353c654f98d4eb709886283f2907bde5eb
Sandro Figo <sandro.figo@gmx.at>
2025-08-27 19:18:50 +0200

    Add verify test

--- KeepAChangelog.IO.Tests/ChangelogTests.cs
+++ KeepAChangelog.IO.Tests/ChangelogTests.cs
@@ -2,9 +2,21 @@ namespace KeepAChangelog.IO.Tests;
 
 public class ChangelogTests
 {
+    static ChangelogTests()
+    {
+        UseSourceFileRelativeDirectory("Data");
+    }
+
     [Fact]
     public void FromFile_ValidChangelog_NoErrors()
     {
-        Changelog.FromFile("Data/valid_changelog.md");
+        Changelog.FromFile("Data/valid_changelog.verified.txt");
+    }
+
+    [Fact]
+    public Task FromFile_ValidChangelog_ToStringIsSameAsOriginal()
+    {
+        Changelog changelog = Changelog.FromFile("Data/valid_changelog.verified.txt");
+        return Verify(changelog.ToString()).UseFileName("valid_changelog");
     }
 }
